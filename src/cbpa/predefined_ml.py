82f8e72cd6615f9a0aae0d63bc1b56581e60bb2b
acash11 <acash11@kent.edu>
2024-12-05 06:07:04 -0500

    Some work on sklearn module

--- src/cbpa/predefined_ml.py
+++ src/cbpa/predefined_ml.py
@@ -2,21 +2,34 @@
 
 from sklearn.cluster import KMeans
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('../../data/processed/test_cluster_data_processed.csv')
 #df = pd.read_csv('../../data/processed/shopping_trends_processed.csv')
 
-clusters_with_ids = (pd.read_csv('../../data/original/test_cluster_data/test_cluster_data.csv'))['Cluster_Label']
-print(clusters_with_ids)
+real_clusters = (pd.read_csv('../../data/original/test_cluster_data/test_cluster_data.csv'))['Cluster_Label']
+print(real_clusters)
 
-print(df)
+#print(df)
 
-df = df.drop('Customer ID', axis='columns')
+def sklearn_ml_kmeans(df):
+    df = df.drop('Customer ID', axis='columns')
+    # Apply K-Means clustering (e.g., 2 clusters)
+    kmeans = KMeans(n_clusters=2)
+    df['Predicted Cluster'] = kmeans.fit_predict(df)
 
-# Apply K-Means clustering (e.g., 2 clusters)
-kmeans = KMeans(n_clusters=2)
-df['Cluster'] = kmeans.fit_predict(df)
+    return(df['Predicted Cluster'])
 
-# Display the DataFrame with the cluster labels
-print(df)
+if __name__ == '__main__':
+    p = sklearn_ml_kmeans(df)
+
+    predicted_clusters = dict()
+    for index, value in enumerate(p):
+        if value not in predicted_clusters:
+            predicted_clusters[value] = np.array(index)
+        else:
+            predicted_clusters[value] = np.append(predicted_clusters[value], index)
+        
+    print(predicted_clusters)
+    print(real_clusters)
 
